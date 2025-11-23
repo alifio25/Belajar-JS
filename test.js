2dc89948ef5f793f010a8a3aeb2216b2b7908775
alifio25 <erenlebong@gmail.com>
2025-11-23 22:04:13 +0700

    Belajar if - else

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
-console.log("Hello World");
+// Basic const, let and printing to console
+// console.log("Hello World");
 
-const nama = "Alifio Eren Atmaja";
-let umur = 30
-console.log(nama);
-console.log(umur);
+// const nama = "Alifio Eren Atmaja";
+// let umur = 30
+// console.log(nama);
+// console.log(umur);
 
-umur = 55
-console.log(umur);
+// umur = 55
+// console.log(umur);
+
+
+// Basic if - else
+const password_fesnuk ="herman777";
+let user_input = "bengkulu777";
+
+if (user_input === password_fesnuk) {
+    console.log("Selamat datang admin suki");
+
+} else {
+    console.log("password salah, coba menyelam lagi")
+
+}
