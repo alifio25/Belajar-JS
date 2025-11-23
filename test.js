d97178743b8b2c4affa5d896fecc8d8a481a841a
alifio25 <erenlebong@gmail.com>
2025-11-23 20:26:52 +0700

    belejar let

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 console.log("Hello World");
 
 const nama = "Alifio Eren Atmaja";
-const umur = 21;
-console.log(nama, umur);
+let umur = 30
+console.log(nama);
+console.log(umur);
+
+umur = 55
+console.log(umur);
