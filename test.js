87564a562b62b57f7729301aa9c3e120e79ebbcc
alifio25 <erenlebong@gmail.com>
2025-11-26 17:45:52 +0700

    mendalami if-else if-else

--- test.js
+++ test.js
@@ -11,13 +11,29 @@
 
 
 // Basic if - else
-const password_fesnuk ="herman777";
-let user_input = "bengkulu777";
+// const password_fesnuk ="herman777";
+// let user_input = "bengkulu777";
 
-if (user_input === password_fesnuk) {
-    console.log("Selamat datang admin suki");
+// if (user_input === password_fesnuk) {
+//     console.log("Selamat datang admin suki");
 
+// } else {
+//     console.log("password salah, coba menyelam lagi")
+
+// }
+
+let nilai_UAS = 73; // use a single numeric score (or parse a specific value from a string)
+
+if (nilai_UAS >= 100) {
+    console.log("nilai A");
+} else if (nilai_UAS >= 90) {
+    console.log("nilai B");
+} else if (nilai_UAS >= 80) {
+    console.log("nilai C");
+} else if (nilai_UAS >= 70) {
+    console.log("nilai D");
+} else if (nilai_UAS >= 60) {
+    console.log("nilai E");
 } else {
-    console.log("password salah, coba menyelam lagi")
-
+    console.log("nilai F");
 }
