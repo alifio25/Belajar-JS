2ee3bfcc93ef65d0310be35e576c3dd0492735aa
alifio25 <erenlebong@gmail.com>
2025-11-26 20:16:45 +0700

    challenge 1 & 2

--- Challenge Agung Hapsah/variabel.js
+++ Challenge Agung Hapsah/variabel.js
@@ -1,2 +1,9 @@
+// Challenge 1
 const nama = "Alifio Eren Atmaja";
 console.log(nama);
+
+// Challenge 2
+const namaDepan ="Alifio";
+const namaTengah ="Eren";
+const namaBelakang ="Atmaja";
+console.log(namaDepan, namaTengah, namaBelakang);
