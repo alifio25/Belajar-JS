69626177bebf236f836f182358ad400f9f2263fc
alifio25 <erenlebong@gmail.com>
2025-11-27 19:10:04 +0700

    challenge 5

--- Challenge Agung Hapsah/variabel-number.js
+++ Challenge Agung Hapsah/variabel-number.js
@@ -1,8 +1,18 @@
 // challenge 4
+// const apel = 5000;
+// const pisang = 10000;
+// const diskon = 10000;
+
+// const pelanggan = apel * 3 + pisang * 2 - diskon;
+
+// console.log(pelanggan);
+
+// Challenge 5
 const apel = 5000;
 const pisang = 10000;
-const diskon = 10000;
+const diskon = 0.1;
 
-const pelanggan = apel * 3 + pisang * 2 - diskon;
+const pelanggan = apel * 3 + pisang * 2;
+const potongan = pelanggan - pelanggan * diskon;
 
-console.log(pelanggan);
+console.log(potongan);
