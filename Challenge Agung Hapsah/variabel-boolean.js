808262d289041f9d94d86371bc8fb22d9de617ab
alifio25 <erenlebong@gmail.com>
2025-11-29 20:21:10 +0700

    challenge 6

---
+++ Challenge Agung Hapsah/variabel-boolean.js
@@ -0,0 +1,12 @@
+// challenge 6
+const punya_anak = false;
+const seorang_pelajar = true;
+const seorang_pekerja = false;
+const bisa_naik_sepeda = true;
+const punya_peliharaan = false;
+
+console.log(punya_anak)
+console.log(seorang_pelajar)
+console.log(seorang_pekerja)
+console.log(bisa_naik_sepeda)
+console.log(punya_peliharaan)
