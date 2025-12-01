3230434a6c239d920cc7329c3549e1b0bf6d41cd
alifio25 <erenlebong@gmail.com>
2025-12-01 17:28:15 +0700

    challenge 10 : comparison operator

---
+++ Challenge Agung Hapsah/comparison.js
@@ -0,0 +1,36 @@
+// challenge 10 : comparison operator
+
+// 1.sama dengan (==)
+// 2.tidak sama dengan (!==)
+// 3.lebih dari (>)
+// 4.kurang dari (<)
+// 5.lebih dari atau sama dengan (>=)
+// 6.kurang dari atau sama dengan (<=)
+
+// soal 1
+if (10 > 5) {
+    console.log("Benar")
+} else {
+    console.log("Salah")
+}
+
+// saol 2
+if (8 < 4) {
+    console.log("Benar")
+} else {
+    console.log("Salah")
+}
+
+// soal 3
+if (-1 >= 2) {
+    console.log("Benar")
+} else {
+    console.log("Salah")
+}
+
+// soal 4
+if (0.5 <= 0) {
+    console.log("Benar")
+} else {
+    console.log("Salah")
+}
