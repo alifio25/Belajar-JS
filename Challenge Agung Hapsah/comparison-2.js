f4261ea54a6d55770de0aaee895cbd35d3a1a056
alifio25 <erenlebong@gmail.com>
2025-12-03 19:18:34 +0700

    challenge 11

---
+++ Challenge Agung Hapsah/comparison-2.js
@@ -0,0 +1,16 @@
+// challenge 11
+// soal 1
+const nilai = 'abs' === 'abc';
+console.log(nilai);
+
+// soal 2
+const boolean = false !== true;
+console.log(boolean);
+
+// soal 3
+const huruf = 'cab' === 'cba';
+console.log(huruf);
+
+// soal 4
+const salah = false === false;
+console.log(salah);
