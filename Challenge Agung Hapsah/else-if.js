50e561b08ece4e4b6441b3a9a7fc499b8fce3bab
alifio25 <erenlebong@gmail.com>
2025-12-04 20:40:29 +0700

    challenge 12 : ELSE if statement

---
+++ Challenge Agung Hapsah/else-if.js
@@ -0,0 +1,14 @@
+// challenge 12 : ELSE IF statement
+const nilai = 5888;
+
+if (nilai < 10) {
+    console.log("satuan");
+} else if (nilai < 100) {
+    console.log("puluhan");
+} else if (nilai < 1000) {
+    console.log("ratusan");
+} else {
+    console.log("ribuan");
+}
+
+// Perhatikan nama filenya!!
