308707ab9fb784d0bb3e04d46f7e50566b942d09
alifio25 <erenlebong@gmail.com>
2025-12-05 20:02:08 +0700

    challenge 13 & 14 : Object

---
+++ Challenge Agung Hapsah/object.js
@@ -0,0 +1,11 @@
+// challenge 13 : Object
+const identitas = {
+    nama: "Alifio Eren Atmaja",
+    umur: 21,
+    tahunlahir: 2005,
+    pekerjaan: false,
+};
+// challenge 14 : console log object menggunakan accessing properties
+// 1.dot notation (.), sudah dipelajari
+// 2.bracket notation ([]), belum dipelajari
+console.log(identitas.nama, "lahir pada tahun", identitas.tahunlahir);
